3acab341bc7af106a36d5f9582e5d0b8e1d44751
ConradMar3 <conradmare890317@gmail.com>
2022-08-09 12:24:28 +0200

    Javascript - alerts, prompt, variables.

--- Web Development/JavaScript ES6/index.js
+++ Web Development/JavaScript ES6/index.js
@@ -1,3 +1,5 @@
+// Chrome -> Source -> Snippets -> + -> index.js:
+
 alert("Hello");
 alert("World.");
 window.alert("Hello World");
---
+++ Web Development/JavaScript ES6/prompt.js
@@ -0,0 +1,17 @@
+// Chrome -> Source -> Snippets -> + -> prompt.js:
+
+prompt("What is your name?");
+// Empty pop-up with input field.
+
+var myName = "Conrad";
+// In console -> type: myName -> display = 'Conrad'
+
+prompt("What is your name?");
+var myName = "Conrad";
+
+alert(myName);
+var yourName = prompt("What is your name?");
+//Input in prompt-box is saved as 'yourName'
+
+alert(" Hello, my name is " + myName + " welcome to my world " + yourName + "!");
+//Output -> "Hello, my name is Conrad welcome to my world John Wick!"

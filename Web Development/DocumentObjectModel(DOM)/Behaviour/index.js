ffd14009ad1931f3d976202d7b124b29b3d6e9ca
ConradMar3 <conradmare890317@gmail.com>
2022-11-10 12:49:54 +0200

    2022/11/10 - add, remove, toggle class - JS.

---
+++ Web Development/DocumentObjectModel(DOM)/Behaviour/index.js
@@ -0,0 +1,13 @@
+// Adding a class of 'invisible' to "btn". CSS = .hidden { visibility: hidden };
+/*
+document.querySelector("button").classList.add("invisible");
+
+// Can also use '.remove' to "btn";
+document.querySelector("button").classList.remove("invisible");
+
+// '.toggle' to class add - "invisible", removes "invisible" when visible and adds "invisible" when not visible;
+document.querySelector("button").classList.toggle("invisible");
+*/
+
+// Changing the color of an element added via JavaScript;
+document.querySelector("h1").classList.add("medium");

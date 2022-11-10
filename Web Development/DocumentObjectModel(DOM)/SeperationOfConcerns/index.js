f354f9cd86116b771ae5bdeffa46b0d3d59f7de6
ConradMar3 <conradmare890317@gmail.com>
2022-11-10 12:30:20 +0200

    2022/11/10 - DOM - Structure, Style and Behave.(JS)

---
+++ Web Development/DocumentObjectModel(DOM)/SeperationOfConcerns/index.js
@@ -0,0 +1,11 @@
+// document.querySelector("button").style.backgroundColor = "yellow";
+
+
+// Adding a class of 'invisible' to "btn". CSS = .hidden { visibility: hidden };
+document.querySelector("button").classList.add("invisible");
+
+// Can also use '.remove' to "btn";
+document.querySelector("button").classList.remove("invisible");
+
+// '.toggle' to class add - "invisible", removes "invisible" when visible and adds "invisible" when not visible;
+document.querySelector("button").classList.toggle("invisible");
